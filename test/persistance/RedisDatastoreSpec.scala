684d1ef2c68e4372cbb9fd3556df03fb724eda81
Donmez, Ugur <ugurdonmez87@gmail.com>
2022-11-10 21:40:25 +0100

    fix tests

--- test/persistance/RedisDatastoreSpec.scala
+++ test/persistance/RedisDatastoreSpec.scala
@@ -1,11 +1,24 @@
 package persistance
 
-import org.scalatestplus.play.PlaySpec
+import org.scalatest._
+import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.play._
+import org.mockito.Mockito._
+import com.redis._
 import persistence.RedisDataStore
+import play.api.Configuration
 
-class RedisDatastoreSpec extends PlaySpec {
+class RedisDatastoreSpec extends PlaySpec with MockitoSugar {
 
-  val dataStore = new RedisDataStore()
+  var mockConfig = mock[Configuration]
+  when(mockConfig.get[String]("redis.host")).thenReturn("host")
+  when(mockConfig.get[String]("redis.host")).thenReturn("port")
+
+  var mockRedis = mock[RedisClient]
+  when(mockRedis.set("key", "value")).thenReturn(true)
+  when(mockRedis.get("key")).thenReturn(Some("value"))
+
+  val dataStore = new RedisDataStore(mockConfig)
 
   "Redis DB " must {
     "store key value" in {
